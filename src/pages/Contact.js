40bcabf6c999abc01c3481cba000e0b2e9bf4edf
mikevampm323 <mike.vartanyan@yahoo.com>
2020-05-27 23:09:57 -0700

    final commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
+import "../index.css";
 
-function Contact() {
-  return <></>;
+class Contact extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: "",
+    message: "",
+  };
+
+  handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    alert("Thank you");
+
+    this.setState({
+      firstName: "",
+      lastName: "",
+      message: "",
+    });
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div>
+        <form className="form">
+          <input
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            className="contact-form"
+            value={this.state.message}
+            name="text"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Message"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Contact;
