e59bfe0d4e7df7f0e45ed5bc1f9aeb9d4d5f9bb9
mikevampm323 <mike.vartanyan@yahoo.com>
2020-05-27 23:13:15 -0700

    Updates

---
+++ precache-manifest.d486590e939c657c53279a75e8825f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc8c649cc039b96bb8eeb7e503a121d",
+    "url": "/react__portfolio/index.html"
+  },
+  {
+    "revision": "6cfb485cec7de5894dfd",
+    "url": "/react__portfolio/static/css/main.b9f6bc7c.chunk.css"
+  },
+  {
+    "revision": "b6fe0d83fd568b5765cc",
+    "url": "/react__portfolio/static/js/2.ff533d73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react__portfolio/static/js/2.ff533d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfb485cec7de5894dfd",
+    "url": "/react__portfolio/static/js/main.ed56d6fa.chunk.js"
+  },
+  {
+    "revision": "1eb8cddd03855ccc8a73",
+    "url": "/react__portfolio/static/js/runtime-main.db9af478.js"
+  }
+]);
